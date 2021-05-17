2d8c9392df6bebaf2e87bb3dbdb9018cb4d9707c
BeachNiger2132 <84304097+BeachNiger2132@users.noreply.github.com>
2021-05-17 00:59:35 -0400

    Update status.go

--- data/status.go
+++ data/status.go
@@ -1,5 +1,7 @@
 package data
 
+import "time"
+
 type MCVersion struct {
 	Name     string  `json:"name"`
 	Protocol float64 `json:"protocol"`
@@ -18,6 +20,8 @@ type Favicon struct {
 type Status struct {
 	Version     *MCVersion `json:"version"`
 	Players     *MCPlayers `json:"players"`
+	Description string     `json:"description"`
 	Favicon     string     `json:"favicon"`
+	Ping        time.Duration
 	Favicons    []Favicon
 }
